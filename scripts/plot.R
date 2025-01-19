6d87f21b8ed4e94986b3c97f8ec5bc8a3cf12f01
Bob Zimmermann <robert.paul.zimmermann@gmail.com>
2025-01-19 18:46:10 -0500

    adding R code

---
+++ scripts/plot.R
@@ -0,0 +1,28 @@
+seqs <- purrr::map(fs::dir_ls("work/utrs_v1/", glob="**.fa"),
+                   Biostrings::readDNAStringSet)
+
+annotation_str <- function(lens) {
+  n_oligos <- purrr::map_int(lens$length, \(x) IRanges::slidingWindows(IRanges::IRanges(1, x), 200, 100) |> _[[1]] |> length()) |> sum()
+  glue::glue("n = {nrow(lens)}\nmedian = {median(lens$length)}\nmean = {signif(mean(lens$length), 2)}\nrange = ({min(lens$length)}, {max(lens$length)})\ntotal length = {sum(lens$length)}\ncalculated num oligos = {n_oligos}")
+}
+
+lens <- purrr::map_int(seqs, \(seq) length(seq[[1]])) |> tibble::enframe( value = "length")
+label <- annotation_str(lens)
+lens |>
+  ggplot2::ggplot(ggplot2::aes(length)) +
+  ggplot2::geom_histogram() + 
+  ggplot2::labs(y = "count") +
+  ggplot2::theme_bw() + 
+  ggplot2::annotate("text", x = 1000, y=75, 
+                    label = label)
+
+
+lens_filt <- lens |> dplyr::filter(length >= 50)
+label <- annotation_str(lens_filt)
+lens_filt |>
+  ggplot2::ggplot(ggplot2::aes(length)) +
+  ggplot2::geom_histogram() + 
+  ggplot2::labs(y = "count") +
+  ggplot2::theme_bw() + 
+  ggplot2::annotate("text", x = 1000, y=75, 
+                    label = label)
