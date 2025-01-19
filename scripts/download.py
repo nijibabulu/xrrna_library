6266fb52b9f9e34812c5f33e7a4d827157d53c8e
Bob Zimmermann <robert.paul.zimmermann@gmail.com>
2025-01-19 18:44:56 -0500

    cleanup

--- scripts/download.py
+++ scripts/download.py
@@ -14,9 +14,7 @@ from ncbi_acc_download.errors import (
 @click.argument("accession_list", type=click.Path(exists=True, path_type=Path))
 @click.argument("output_dir", type=click.Path(exists=True, path_type=Path))
 def main(accession_list, output_dir, molecule, format, api_key):
-    config = ncbi_acc_download.core.Config(
-        molecule=molecule, format=format, api_key=api_key
-    )
+    config = ncbi_acc_download.core.Config(molecule=molecule, format=format, api_key=api_key)
     with open(accession_list) as f, open(output_dir / "err.txt", "w") as log:
         for line in f:
             accession = line.strip()
@@ -25,10 +23,10 @@ def main(accession_list, output_dir, molecule, format, api_key):
                 ncbi_acc_download.core.download_to_file(accession, config, out_path)
             except InvalidIdError as _:
                 print(f"Invalid ID: {accession}")
-                log.write("{accession}\tinvalid ID")
+                log.write(f"{accession}\tinvalid ID")
             except DownloadError as _:
                 print(f"Download error: {accession}")
-                log.write("{accession}\tdownload error")
+                log.write(f"{accession}\tdownload error")
 
 
 if __name__ == "__main__":
--- scripts/parse.py
+++ scripts/parse.py
@@ -1,29 +1,27 @@
 from pathlib import Path
 import click
-from Bio import SeqIO, SeqFeature
+from Bio import SeqIO, SeqRecord, SeqFeature
+
+
+def write_utr3(rec: SeqRecord, feat: SeqFeature, out_stem: str, output_dir: Path):
+    utr_rec = feat.extract(rec)
+    range_str = f"{feat.location.start}-{feat.location.end}"
+    utr_rec.description += f" 3'UTR {range_str}"
+    out_path = output_dir / f"{out_stem}_{range_str}.fa"
+    SeqIO.write(utr_rec, out_path, "fasta")
 
 
 @click.command()
-@click.argument(
-    "input_dir", type=click.Path(exists=True, file_okay=False, path_type=Path)
-)
-@click.argument(
-    "output_dir", type=click.Path(exists=True, file_okay=False, path_type=Path)
-)
+@click.argument("input_dir", type=click.Path(exists=True, file_okay=False, path_type=Path))
+@click.argument("output_dir", type=click.Path(exists=True, file_okay=False, path_type=Path))
 def main(input_dir: Path, output_dir: Path):
-    for file in input_dir.iterdir():
-        if file.suffix == ".gbk":
-            rec = SeqIO.read(file, "genbank")
-            utr3_feats = [feat for feat in rec.features if feat.type == "3'UTR"]
-            for feat in utr3_feats:
-                utr_rec = feat.extract(rec)
-                range_str = f"{feat.location.start}-{feat.location.end}"
-                utr_rec.description += f" 3'UTR {range_str}"
-                SeqIO.write(
-                    utr_rec,
-                    output_dir / f"{file.stem}_{range_str}.fa",
-                    "fasta",
-                )
+    _ = [
+        write_utr3(rec, feat, file.stem, output_dir)
+        for file in input_dir.glob("*.gbk")
+        for rec in SeqIO.parse(file, "genbank")
+        for feat in rec.features
+        if feat.type == "3'UTR"
+    ]
 
 
 if __name__ == "__main__":
